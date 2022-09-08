a8dbec704c0ec82c7159e5aed9c7bb3f5a8a5fcc
Cubites <75084369+Cubites@users.noreply.github.com>
2022-09-08 17:11:34 +0900

    서버 cors 설정

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 app.set('port', 4000);
+
+const corsOptions = {
+    origin: 'http://localhost:4000',
+    credentials: true
+};
+app.use(cors(corsOptions));
 
 app.post('/api/main', (req, res) => {
     res.status(200).send({success: true, contents: 'this is Main page'});
