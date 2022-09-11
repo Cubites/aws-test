51e3ed718a6edb6c756eb8e5879b71384c5d106a
Cubites <75084369+Cubites@users.noreply.github.com>
2022-09-11 14:31:07 +0900

    Update Main.jsx

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -18,7 +18,8 @@ const Main = () => {
   }
 
   const RequestServer = async () => {
-    const url = process.env.REACT_APP_NODE_ENV === 'production' ? `http://${process.env.REACT_APP_AWS_IP}:4000/api/main` : '/api/main';
+    // const url = process.env.REACT_APP_NODE_ENV === 'production' ? `http://${process.env.REACT_APP_AWS_IP}:4000/api/main` : '/api/main';
+    const url = `http://${process.env.REACT_APP_AWS_IP}:4000/api/main`;
     console.log('url : ', url);
     try{
       const answer = await axios.post(url);
